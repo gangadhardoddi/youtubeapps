a0bd1b148dae30510091ae979bb7c6f11f364a76
Andrew Machnew <amachnew@emp-game.com>
2015-02-10 13:09:12 +0200

    fixes

---
+++ ChalkableBaseAppLib/ChalkableBaseAppLib/Models/Dto/AnnouncementApplicationDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChalkableBaseAppLib.Models.Dto
+{
+    public class AnnouncementApplicationData
+    {
+        public Guid id { get; set; }
+        public int applicationid { get; set; }
+        public int announcementid { get; set; }
+        public int? schoolpersonid { get; set; }
+        public bool active { get; set; }
+        public string name { get; set; }
+    }
+
+    public class AnnouncementApplicationDto
+    {
+        public AnnouncementApplicationData data { get; set; }
+    }
+}
