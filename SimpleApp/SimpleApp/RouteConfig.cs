168f119d49b16ad6f117ae02364171b72bcc30fc
Andriy Machnew <Andriy Machnew@localhost>
2015-06-26 17:26:08 +0300

    added simple app to repository

---
+++ SimpleApp/SimpleApp/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Wrapper
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "NewRout",
+                "Wrapper/{*url}",
+                new
+                    {
+                        controller = "Home",
+                        action = "Index",
+                    }
+                
+           );
+            routes.MapRoute(
+               
+                name: "Default",
+                url: "{controller}/{action}/{url}",
+                defaults: new { controller = "Home", action = "Index", url = UrlParameter.Optional }
+            );
+        }
+    }
+
+}
