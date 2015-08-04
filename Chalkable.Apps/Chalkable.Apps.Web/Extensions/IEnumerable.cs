9a7a5f638d2cc3a27349801f4e3dd1e91f5c984f
Jonathan Whitehurst <jwhitehurst@chalkable.com>
2015-08-04 11:34:16 -0500

    Generic "simple app" wrapper

---
+++ Chalkable.Apps/Chalkable.Apps.Web/Extensions/IEnumerable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chalkable.Apps.Web.Extensions {
+    public static class IEnumerable {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable) {
+            return enumerable == null || !enumerable.Any();
+        }
+    }
+}
---
+++ Chalkable.Apps/Chalkable.Apps.Web/Models/ApplicationList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chalkable.Apps.Entities;
+using Chalkable.Apps.Entities.Models;
+
+namespace Chalkable.Apps.Web.Models {
+    public class ApplicationList {
+
+        private static readonly Lazy<IEnumerable<SimpleApplication>> _applications = new Lazy<IEnumerable<SimpleApplication>>(() => {
+            var dbContext = new Chalkable.Apps.Entities.ChalkableAppsContext();
+            return dbContext.SimpleApplications.ToList();
+            
+        });
+
+        public static IEnumerable<SimpleApplication> Applications {
+            get { return _applications.Value; }
+        }
+    }
+}
